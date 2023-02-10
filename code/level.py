de984aec1936e6c696aa421ab41b2d22ab9b0f07
kristian-girsang <kristian.girsang@ui.ac.id>
2023-02-10 19:55:43 +0700

    setup level and import game data from csv

---
+++ code/level.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Level:
+    def __init__(self, level_data,surface):
+        self.display_surface = surface
+    
+    def run(self):
+        #Run the entire level
+        pass
