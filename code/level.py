71190353aa018353ab23cf017471ebab9c594494
kristian-girsang <kristian.girsang@ui.ac.id>
2023-02-12 20:56:25 +0700

    convert csv data to usable positions

--- code/level.py
+++ code/level.py
@@ -1,14 +1,36 @@
 import pygame
 from support import import_csv_layout
+from settings import tile_size
+from tiles import Tile
 
 
 class Level:
     def __init__(self, level_data, surface):
+        # general setup
         self.display_surface = surface
+        self.world_shift = 0
 
+        # terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
-        print(terrain_layout)
+        self.terrain_sprites = self.create_tile_group(
+            terrain_layout, 'terrain')
+
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        sprite = Tile(tile_size, x, y)
+                        sprite_group.add(sprite)
+
+        return sprite_group
 
     def run(self):
         # Run the entire level
-        pass
+        self.terrain_sprites.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
